1cf5934feea51efa29c764c4bbb358a18216072d
Kike Algarra <kike@areavisual.net>
2019-09-14 11:56:50 +0200

    SwiftHolaXR
    ejemplo de programa en Swift para ir extendiendo el código.

---
+++ SwiftHolaXR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftHolaXR
+//
+//  Created by bykike on 14/09/2019.
+//  Copyright © 2019 bykike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 60, width: view.bounds.width, height: 50))
+        label.text = "Hola mundo"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        label.textColor = UIColor.darkText
+        view.addSubview(label)
+    }
+
+
+}
+
